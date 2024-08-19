956cf1a127d83f5ec3ee3c7e5c587d4ca287fc00
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-19 11:46:08 +0200

    Add files via upload

---
+++ for-loop-1-to-1000-through-10.cpp
@@ -0,0 +1,16 @@
+// FOR LOOP FOR NUMBERS FROM 0 TO 1000 THROUGH 10 CODE
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+    int number;
+
+    for (int number = 0; number <= 1000; number += 10) {
+        std::cout << number << '\n';
+    }
+
+    return 0;
+
+}
