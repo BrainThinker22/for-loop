7482f9d87c732fa3e298743120586cff72d0ef9d
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-19 11:42:22 +0200

    Add files via upload

---
+++ for-loop-test.cpp
@@ -0,0 +1,108 @@
+/*
+
+        // SIMPLE FOR LOOP NUMBERS FROM 1 TO 100 CODE
+
+    #include <iostream>
+    #include <string>
+
+    int main()
+{
+    int number;
+
+    for (int number = 1; number <= 100; number++) {
+        std::cout << number << '\n';
+    }
+
+    return 0;
+
+}
+
+*/
+
+/*
+
+        // FOR LOOP FOR NUMBERS FROM 0 TO 1000 THROUGH 10 CODE
+
+    #include <iostream>
+    #include <string>
+
+    int main() {
+
+    int number;
+
+    for (int number = 0; number <= 1000; number += 10) {
+        std::cout << number << '\n';
+    }
+
+    return 0;
+
+}
+
+*/
+
+/*
+
+
+        // FOR LOOP FOR NUMBERS FROM 0 TO 1000000 THROUGH 100 CODE
+
+    #include <iostream>
+    #include <string>
+
+    int main() {
+
+    int number;
+
+    for (int number = 0; number <= 1000000; number += 100) {
+        std::cout << number << '\n';
+    }
+
+    return 0;
+}
+
+*/
+
+/*
+
+        // USAGE OF BREAK COMMAND
+
+    #include <iostream>
+    #include <string>
+
+    int main() {
+
+    int number;
+
+    for (int number = 0; number <= 100; number++) {
+        if (number == 50) {
+            break;
+        }
+        std::cout << number << '\n';
+    }
+
+    return 0;
+}
+
+*/
+
+/*
+
+        // USAGE OF CONTINUE COMMAND
+
+    #include <iostream>
+    #include <string>
+
+    int main() {
+
+    int number;
+
+    for (int number = 0; number <= 100; number++) {
+        if (number == 50) {
+            continue;
+        }
+        std::cout << number << '\n';
+    }
+
+    return 0;
+}
+
+*/
