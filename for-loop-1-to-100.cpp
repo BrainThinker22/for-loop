6425913ea973f34fa7be3b4f23b7f1907c47c484
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-19 11:45:01 +0200

    Add files via upload

---
+++ for-loop-1-to-100.cpp
@@ -0,0 +1,16 @@
+// SIMPLE FOR LOOP NUMBERS FROM 1 TO 100 CODE
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int number;
+
+    for (int number = 1; number <= 100; number++) {
+        std::cout << number << '\n';
+    }
+
+    return 0;
+
+}
